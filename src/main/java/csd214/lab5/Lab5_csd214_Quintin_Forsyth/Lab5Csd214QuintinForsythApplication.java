35bc6c261ef7aaa00235e4223432caf2852827de
QuintinForsyth1 <22027564@saultcollege.ca>
2024-11-19 16:32:11 -0500

    done lab5

---
+++ src/main/java/csd214/lab5/Lab5_csd214_Quintin_Forsyth/Lab5Csd214QuintinForsythApplication.java
@@ -0,0 +1,13 @@
+package csd214.lab5.Lab5_csd214_Quintin_Forsyth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5Csd214QuintinForsythApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab5Csd214QuintinForsythApplication.class, args);
+	}
+
+}
---
+++ src/test/java/csd214/lab5/Lab5_csd214_Quintin_Forsyth/Lab5Csd214QuintinForsythApplicationTests.java
@@ -0,0 +1,13 @@
+package csd214.lab5.Lab5_csd214_Quintin_Forsyth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5Csd214QuintinForsythApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
